99047e2976a543eb2231f1f62c19204cb0a97e5f
Raul Souza <xlraullx9@hotmail.com>
2025-01-17 16:22:04 -0300

    adicionado exercício número 2 JS básico

---
+++ exercicios-js-basicos/02-boleto-pago/index.js
@@ -0,0 +1,9 @@
+const boletoPago = false
+
+/*if (boletoPago){
+    alert("O boleto está pago!")
+} else if (boletoPago === false){
+    alert("O boleto não está pago!")
+}*/
+
+boletoPago ? alert("O boleto está pago") : alert("O boleto não está pago")
