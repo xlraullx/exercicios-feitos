01fb6afd57afb350469760df2bdc83b09056dbac
Raul Souza <xlraullx9@hotmail.com>
2025-01-17 16:00:44 -0300

    Adicionado nova pasta com o primeiro exercicio de JS básico

---
+++ exercicios-js-basicos/01-mensagem-de-bem-vindo/index.js
@@ -0,0 +1,8 @@
+let Nome = prompt("Informe seu nome")
+
+if (Nome === ""){
+    Nome = "visitante"
+    alert("Seja bem vindo(a), " + Nome + " !")
+} else {
+    alert("Seja bem vindo(a), " + Nome + " !")
+}
